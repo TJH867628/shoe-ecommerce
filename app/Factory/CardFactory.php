e6230203ba5361e9898c55e1de6384c865a9ddb5
Tan Jing Heng <104840897+TJH867628@users.noreply.github.com>
2026-06-19 22:37:44 +0800

    Update CardFactory.php

--- app/Factory/CardFactory.php
+++ app/Factory/CardFactory.php
@@ -4,7 +4,7 @@ namespace App\Factory;
 
 use App\Payments\Payment;
 use App\Payments\CardPayment;
-
+//concrete creator
 class CardFactory extends PaymentFactory
 {
     public function createPayment(): Payment
